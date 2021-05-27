c47cbf9b3e891e3a4809c4e738105c169424a740
DESKTOP-HD00NDV\assam <assamoi.josias@gmail.com>
2021-05-27 16:11:36 +0200

    deployment v1

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,13 @@
 module.exports = (api) => {
   api.cache(true)
 
-  const presets = [
-    '@babel/preset-env',
-    '@babel/preset-typescript',
+  const presets = ['@babel/typescript', '@babel/env']
+  const plugins = [
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/transform-runtime',
   ]
 
-  return { presets }
+  return { presets, plugins }
 }
