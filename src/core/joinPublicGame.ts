bcdffe8505f4bd4263eeffaacc0d5c1023962ba3
FacinouFabian <fabianfacinou02@gmail.com>
2021-05-28 09:46:14 +0200

    added user avatar data

--- src/core/joinPublicGame.ts
+++ src/core/joinPublicGame.ts
@@ -12,7 +12,7 @@ const joinPublicGame = async ({ username, user_uuid, themeId }, io, socket) => {
   if (game) {
     await query(`INSERT INTO user_games (user_uuid,game_id) VALUES ($1,$2)`, [user_uuid, game.id])
 
-    const avatar = await query(`SELECT * FROM avatars WHERE user_uuid=$1`, [user_uuid])
+    const avatar = await query(`SELECT * [except id,user_uuid] FROM avatars WHERE user_uuid=$1`, [user_uuid])
 
     socket.join(game.id)
 
