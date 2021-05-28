d437c30e6006930799b0820b46e64c91c860aa95
FacinouFabian <fabianfacinou02@gmail.com>
2021-05-28 12:43:39 +0200

    added user avatar data

--- src/core/startGame.ts
+++ src/core/startGame.ts
@@ -16,6 +16,7 @@ const startGame = async ({ user_uuid, gameId }, io, socket) => {
   } else {
     // match.state = "started";
     await query('UPDATE games SET state=$1 WHERE id=$2', ['started', gameId])
+    socket.emit('start')
     io.to(gameId).emit('start')
   }
 }
